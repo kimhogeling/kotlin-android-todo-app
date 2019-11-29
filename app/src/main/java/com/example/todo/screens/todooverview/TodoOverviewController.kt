27e4783e1f775d886290d512003e0710d4f65017
Kim Hogeling <kim.hogeling@cognizant.com>
2019-11-29 16:57:00 +0100

    removes debug logs

--- app/src/main/java/com/example/todo/screens/todooverview/TodoOverviewController.kt
+++ app/src/main/java/com/example/todo/screens/todooverview/TodoOverviewController.kt
@@ -1,6 +1,5 @@
 package com.example.todo.screens.todooverview
 
-import android.util.Log
 import com.example.todo.GetTodoUseCase
 
 class TodoOverviewController(private val getTodoUseCase: GetTodoUseCase) :
@@ -11,14 +10,12 @@ class TodoOverviewController(private val getTodoUseCase: GetTodoUseCase) :
 
     override fun onAddTodo(todo: String) {
         todoList.add(todo)
-        Log.i("onAddTodo", todoList.toString())
         todoOverviewView.setTodos(todoList.toList())
     }
 
     fun onCreate() {
         todoOverviewView.registerListener(this)
         todoList.addAll(getTodoUseCase.getTodos())
-        Log.i("onCreate", todoList.toString())
         todoOverviewView.setTodos(todoList.toList())
     }
 
